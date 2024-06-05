0e399789020e4ebf060146982149c3b414ca8fb8
Rahul Sharma <rahulbond2004@gmail.com>
2024-06-05 20:37:17 +0530

    created road

--- main.js
+++ main.js
@@ -2,14 +2,24 @@ const canvas = document.getElementById("MyCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2, canvas.width);
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
 Animate();
 
 function Animate() {
   car.update();
+
   canvas.height = window.innerHeight;
+
+  ctx.save();
+
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
+
   requestAnimationFrame(Animate);
 }
