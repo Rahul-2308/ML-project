f2e010037f00239ed0ddb9e521800866587b086a
Rahul Sharma <rahulbond2004@gmail.com>
2024-06-05 13:03:59 +0530

    added simple physics

---
+++ car.js
@@ -0,0 +1,51 @@
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+
+        this.speed=0;
+        this.acceleration=0.1;
+        this.MaxSpeed=5;
+        this.friction=0.02;
+
+        this.controls=new Controls(); 
+    }
+
+    update(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+        if(this.speed>this.MaxSpeed) {
+            this.speed=this.MaxSpeed;
+        }
+        if(this.speed<-this.MaxSpeed/2) {
+            this.speed=-this.MaxSpeed/2;
+        }
+        if(this.speed>0) {
+            this.speed-=this.friction;
+        }
+        if(this.speed<0) {
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction) {
+            this.speed=0;
+        }
+        this.y-=this.speed;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(
+            this.x-this.width/2,
+            this.y-this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+    }
+}
