d87139e98c04727c7f5319906e0a848d75c2703c
Rahul Sharma <rahulbond2004@gmail.com>
2024-06-09 19:33:02 +0530

    added borders

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("MyCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2, canvas.width);
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width/2, canvas.width*0.85);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 Animate();
 
